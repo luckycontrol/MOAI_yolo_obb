eb8326613c2e7f037e2fd67539e5fa170ca539f5
luckycontrol <beansu1@naver.com>
2025-02-04 15:59:46 +0900

    inference 코드 수정

--- test.py
+++ test.py
@@ -12,21 +12,22 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--project", default="20250115", type=str)
-    parser.add_argument("--subproject", default="test_sub", type=str)
-    parser.add_argument("--task", default="test_obb", type=str)
-    parser.add_argument("--version", default="v1", type=str)
+    parser.add_argument("--project", default="20250123", type=str)
+    parser.add_argument("--subproject", default="sub", type=str)
+    parser.add_argument("--task", default="obb", type=str)
+    parser.add_argument("--version", default="v3", type=str)
     args = parser.parse_args()
     return args
 
 def main(args):
     manager = Manager(**vars(args))
 
-    weight_path = manager.get_best_weight_path()
     hyp_yaml_path = manager.get_hyp_yaml_path()
     with open(hyp_yaml_path, "r") as f:
         hyp = yaml.safe_load(f)
 
+    weight_path = manager.get_best_weight_path()
+    print(weight_path)
     model = YOLO(weight_path)
 
     ARGS = {
@@ -45,45 +46,49 @@ def main(args):
 
     inference_results = model.predict(**ARGS)
 
-    for result in inference_results:
-        names = result.names
-        classes = result.obb.cls.cpu().numpy().tolist()
-        confs = result.obb.conf.cpu().numpy().tolist()
-        
-        # 검출 결과 가져오기
-        xywhr = result.obb.xywhr  # shape = [N, 5]
+    save_path = f"{manager.location}/{manager.project}/{manager.subproject}/{manager.task}/{manager.version}/inference_result"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
 
+    for result in inference_results:
         # Prepare filename and save path
         filename = os.path.basename(result.path)
         base, _ = os.path.splitext(filename)
         new_filename = base + '.txt'
-        save_path = f"{manager.location}/{manager.project}/{manager.subproject}/{manager.task}/{manager.version}/inference_result"
+        classes = result.names
 
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-            
-        if xywhr.shape[0] == 0:
+        obb_results = []
+
+        for i in range(len(result.obb)):
+            cls_name = classes[int(result.obb.cls.cpu().numpy().tolist()[i])]
+            conf = result.obb.conf.cpu().numpy().tolist()[i]
+            xywhr = result.obb.xywhr.cpu().numpy().tolist()[i]  # shape = [N, 5]
+
+            obb_results.append([cls_name, conf, xywhr])
+
+        if len(obb_results) == 0:
             # Create empty file when no OBB is found
             open(os.path.join(save_path, new_filename), 'w').close()
             continue
         
         result_content = []
         # 여러 바운딩 박스를 순회하며 그리기
-        for i in range(xywhr.shape[0] - 1):
-            # 텐서 -> CPU -> numpy 변환
-            cx, cy, w, h, r = xywhr[i].cpu().numpy().tolist()
+        for i in range(len(obb_results)):
+            cls_name = obb_results[i][0]
+            conf = obb_results[i][1]
+            cx, cy, w, h, r = obb_results[i][2]
 
             # w, h가 음수/영인 경우 방어적 처리
             if w <= 0 or h <= 0:
                 continue
 
             result_content.append([
-                names[classes[i]],
+                cls_name,
                 cx, cy, w, h, r,
-                confs[i]
+                conf
             ])
 
-        with open(save_path + "/" + new_filename, "w") as f:
+        with open(os.path.join(save_path, new_filename), "w") as f:
             for content in result_content:
                 f.write(f"{content[0]} {content[1]} {content[2]} {content[3]} {content[4]} {content[5]} {content[6]}\n")
 
